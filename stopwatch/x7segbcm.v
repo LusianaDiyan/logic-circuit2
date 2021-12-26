f160a6a467c9eca55c5c9117950eb4e8b6bad67c
Lusiana Diyan N <36321080+LusianaDiyan@users.noreply.github.com>
2021-12-26 20:00:49 +0700

    up comms

---
+++ stopwatch/x7segbcm.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:02:35 05/13/2019 
+// Design Name: 
+// Module Name:    x7segbcm 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module x7segbcm(
+    );
+
+
+endmodule
